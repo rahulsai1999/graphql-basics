fef52756fd175632e1ff8c03e756f407a89f5cb5
rahulsai1999 <noneuser2183@gmail.com>
2020-03-25 18:47:17 +0530

    simple query and mutation

---
+++ 2. Backend Node/src/index.js
@@ -0,0 +1,39 @@
+import { GraphQLServer } from "graphql-yoga";
+
+let links = [
+  {
+    id: "link-0",
+    url: "www.howtographql.com",
+    description: "Fullstack tutorial for GraphQL"
+  }
+];
+
+//2
+let idCount = links.length;
+
+const resolvers = {
+  Query: {
+    info: () => `This is the API of a Hackernews Clone`,
+    feed: () => links
+  },
+  Mutation: {
+    // 2
+    post: (parent, args) => {
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url
+      };
+      links.push(link);
+      return link;
+    }
+  }
+};
+
+//3
+const server = new GraphQLServer({
+  typeDefs: "./src/schema.graphql",
+  resolvers
+});
+
+server.start(() => console.log(`Server running on http://localhost:4000`));
