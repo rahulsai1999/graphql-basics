d1806b9bd0af45b8e8a8db1f632e0c5f463a4fed
rahulsai1999 <noneuser2183@gmail.com>
2020-03-27 12:41:34 +0530

    count functionality

--- 2. Backend Node/src/resolvers/Query.js
+++ 2. Backend Node/src/resolvers/Query.js
@@ -14,7 +14,16 @@ async function feed(parent, args, context, info) {
     first: args.first,
     orderBy: args.orderBy
   });
-  return links;
+  const count = await context.prisma
+    .linksConnection({
+      where
+    })
+    .aggregate()
+    .count();
+  return {
+    links,
+    count
+  };
 }
 
 export { feed };
