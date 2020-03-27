34120365a438ad18c14fd696694b7931c76c5567
rahulsai1999 <noneuser2183@gmail.com>
2020-03-27 12:28:27 +0530

    filtering

--- 2. Backend Node/src/resolvers/Query.js
+++ 2. Backend Node/src/resolvers/Query.js
@@ -1,5 +1,17 @@
-const feed = (parent, args, context, info) => {
-  return context.prisma.links();
-};
+async function feed(parent, args, context, info) {
+  const where = args.filter
+    ? {
+        OR: [
+          { description_contains: args.filter },
+          { url_contains: args.filter }
+        ]
+      }
+    : {};
+
+  const links = await context.prisma.links({
+    where
+  });
+  return links;
+}
 
 export { feed };
